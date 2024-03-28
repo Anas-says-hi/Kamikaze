682281c13d656427c143235c741156f67ccf4ffc
Anas-says-hi <anas.siddiquewasfound@gmail.com>
2024-03-28 23:08:43 +0600

    destroying enemy

--- main.lua
+++ main.lua
@@ -14,6 +14,12 @@ function love.load()
   enemy_g = anim8.newGrid(49, 39, enemy_image:getWidth(), enemy_image:getHeight())
   enemy_animation = anim8.newAnimation(enemy_g('1-7',1), 0.05)
 
+  --explosion
+  exp_image = love.graphics.newImage('sprites/explosion.png')
+  exp_g = anim8.newGrid(64, 64, exp_image:getWidth(), exp_image:getHeight())
+  exp_animation = anim8.newAnimation(exp_g('1-9',1), 0.07, "pauseAtEnd")
+
+
   --bullets
   bullets = {}
   bulletSpeed = 10
@@ -22,6 +28,9 @@ function love.load()
   enemies = {}
   enemies[1] = {x = 0,y = 0}
 
+  explosion = {}
+  explosion.x = -200
+  explosion.y = -200
   --player
   player = {}
   player.x = (800/2 - 50)/2
@@ -31,17 +40,18 @@ function love.load()
   player.accel = 0.5
   player.friction = 0.08
   
-  enemey_spawner_time = love.timer.getTime()
-  currTime = love.timer.getTime()+ 2
+  currTime = love.timer.getTime()
+  step = love.timer.getTime() + 2
+
 end
 
 function love.update(dt)
 
   --spawning enrmies
-  enemey_spawner_time = love.timer.getTime()
+  currTime = love.timer.getTime()
 
-  if enemey_spawner_time >= currTime then
-    currTime = love.timer.getTime() + 2
+  if currTime >= step then
+    step = love.timer.getTime() + 2
     table.insert(enemies, {x= math.random(0, (800 - 80)/2), y = -39})
   end
   
@@ -84,6 +94,22 @@ end
     enemy.y = enemy.y + 1
   end
 
+  --check collision with bullets
+  for _, bullet in ipairs(bullets) do
+    for _, enemy in ipairs(enemies) do
+      if CheckCollision(bullet.x, bullet.y, bullet.width, bullet.height, enemy.x, enemy.y, 49, 39) then
+        explosion.x = enemy.x
+        explosion.y = enemy.y
+        exp_animation:gotoFrame(1)
+        exp_animation:resume()
+        exploded = true
+
+        table.remove(bullets, _)
+        table.remove(enemies, _)
+      end
+    end
+  end
+
 
   --bullets
   for _, bullet in ipairs(bullets) do
@@ -96,12 +122,18 @@ end
   --animations
   player_animation:update(dt)
   enemy_animation:update(dt)
+  exp_animation:update(dt)
+  
+
+  -- exp_animation:pauseAtEnd()
 
 end
 
 function love.draw()
   love.graphics.scale(2)
   love.graphics.setBackgroundColor(23/255,32/255,56/255)
+
+  exp_animation:draw(exp_image, explosion.x, explosion.y)
 
   for _, bullet in ipairs(bullets) do
     love.graphics.rectangle("fill", bullet.x, bullet.y, bullet.width, bullet.height)
@@ -124,3 +156,10 @@ function love.keypressed(key, scancode, isrepeat)
       table.insert(bullets, {x = player.x +  49/2,y = player.y + 10,width = 1,height = 10})
     end
  end
+
+ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
